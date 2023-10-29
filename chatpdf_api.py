84af758d7c8f8a63cac873964772d8945d72dd6e
mrazmartin <mraz.martin.cz@gmail.com>
2023-10-29 11:33:06 +0100

    generalization

--- chatpdf_api.py
+++ chatpdf_api.py
@@ -1,9 +1,11 @@
-
+import os
 import requests
+from chat_prompt import gpt_api
 
 DEFAULT_CONTEXT = ""
 
-JSON_STYLE = """Provide the results in the following json format:
+JSON_STYLE = """Put everything in json. Provide the results in the following json format:
+    {
     "question": "What is the key difference between Python 2 and Python 3?",
     "options": [
         "AAA",
@@ -13,27 +15,36 @@ JSON_STYLE = """Provide the results in the following json format:
       ],
     "correct_answer_id": 2,
     "explanation": short explanation of the correct answer in one short sentence,
-    "topic": 
+    "topic":
+    }
 """
 
 class chatpdf_api:
-    def __init__(self, key=None) -> None:
+    def __init__(self, key=None, pdf_path="", key_path="") -> None:
 
         self.debug_mode = False
-        self.key = key
+        if key is None:
+            self.key = self.read_key_from_file(key_path)
+        else:
+            self.key = key 
         self.context = DEFAULT_CONTEXT
-        self.pdf_source_id = None
+        self.pdf_source_id = self.upload_pdf(pdf_path)
+        print(self.pdf_source_id)
 
-    def read_key_from_file(self, file_name="chatpdf_key.txt"):
+    def read_key_from_file(self, file_name=""):
         try:
             with open(file_name, 'r') as file:
-                self.key = file.readline()
-            if self.debug_mode:
-                print(self.key)
+                if self.debug_mode:
+                    print(self.key)
+                return file.readline()
         except:
             print("ERROR: key file doesnt exist")
     
     def upload_pdf(self, pdf_path):
+
+        if not os.path.isfile(pdf_path):
+            raise ValueError("No pdf to be uploaded!")
+
         files = [('file', ('file', open(pdf_path, 'rb'), 'application/octet-stream'))]
         headers = {'x-api-key': self.key}
 
@@ -41,12 +52,13 @@ class chatpdf_api:
             'https://api.chatpdf.com/v1/sources/add-file', headers=headers, files=files)
         
         if response.status_code == 200:
-            print('Source ID:', response.json()['sourceId'])
+            if self.debug:
+                print('Source ID:', response.json()['sourceId'])
+            return response.json()['sourceId']
         else:
             print('Status:', response.status_code)
             print('Error:', response.text)
-        
-        self.pdf_source_id = response.json()['sourceId']
+            exit()
 
     def debug(self, value=True):
         self.debug_mode =  value
@@ -54,14 +66,13 @@ class chatpdf_api:
     def create_prompt(self, question, rqst_sources=False):
         
         tmp_rqst = {"referenceSources": rqst_sources,
-         "sourceId": tmp_api.pdf_source_id,
+         "sourceId": self.pdf_source_id,
          "messages": [
             {
             "role": "user",
             "content": f"{question}"
             }]
         }
-        
         return tmp_rqst
 
     def get_topics(self, max_topics=3):
@@ -77,7 +88,6 @@ class chatpdf_api:
 
     def ask_prompt(self, query=None):
         headers = {'x-api-key': self.key, "Content-Type": "application/json",}
-
         response = requests.post(
             'https://api.chatpdf.com/v1/chats/message', headers=headers, json=query)
 
@@ -93,16 +103,45 @@ class chatpdf_api:
 
     def test_run(self):
         tmp_api.debug()
-        tmp_api.read_key_from_file()
-        tmp_api.upload_pdf("provided_file.pdf")
-        print("\nPDF upload succ\n")
+        #tmp_api.upload_pdf("provided_file.pdf")
         
+        if self.debug:
+            print("Start: get topics")
         topics = tmp_api.get_topics(5)
         
-        question = f"Give me a question regarding the provided topics {topics} from the pdf file. {JSON_STYLE}."
+        question = f"Give me a multiple choice question regarding the provided topics {topics} from the pdf file. {JSON_STYLE}."
         tmp_rqst = tmp_api.create_prompt(question, rqst_sources=True)
 
         tmp_api.ask_prompt(tmp_rqst)
 
-tmp_api = chatpdf_api()
+class any_api():
+    def __init__(self, api_service=None) -> None:
+        self.service = api_service
+
+    def init_question(self, profile, pdf, opt_context):
+        if os.path.isfile(pdf):
+            self.service = chatpdf_api()
+        else:
+            self.service = gpt_api()
+    
+    def reinfoce_topic(self, topic=""):
+        # generate more questions about a given topic
+        pass
+    
+    def reinfoce_auto(self):
+        # generate more questions by default
+        pass
+
+tmp_api = chatpdf_api(pdf_path="provided_file.pdf", key_path="chatpdf_key.txt")
 tmp_api.test_run()
+
+# comes from front end
+profile_background = "junior developer"
+profile_aim = "learn python" 
+profile = f"I am a {profile_background}. I want to {profile_aim}"
+pdf = "provided_file.pdf"
+opt_context = ""  
+
+# run question creator
+our_api = any_api()
+our_api.init_question(profile=profile, pdf_path=pdf, opt_context=opt_context)
